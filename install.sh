ad2ac72dd01760305964243bdf3818de553d8672
Wasin Thonkaew <haxpor@gmail.com>
2018-08-14 20:18:04 +0800

    Improve install script and add template file. New Hint.

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 # don't use ls, it's broken and will return unexpected number of files than we need
-FILES=($(find ./src -type f -exec basename {} \;))
+FILES=($(find ./src -type f -name '*.3' -not -name 'SDL_Template.3' -exec basename {} \;))
 TARGET=/usr/local/share/man/man3
 WORKING_DIR=$(pwd)
 
 for f in "${FILES[@]}"; do
-	ln -s "$WORKING_DIR/src/$f" "$TARGET/$f"
+	echo "Create symlink for $f at $TARGET/$f"
+	ln -sf "$WORKING_DIR/src/$f" "$TARGET/$f"
 done
+
+echo "done"
+
